2bf538adabe6a70229eb45e9838459323f48dd7b
Vladimir Mechkauskas <vmechkauskas@gmail.com>
2022-01-04 16:20:41 +0200

    - fixed comments

--- src/services/hooks.ts
+++ src/services/hooks.ts
@@ -1,13 +1,8 @@
 import { useState, useCallback } from 'react';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
-import type { RootState, AppDispatch } from '../constants/store';
 
 import { getQueryStringValue, setQueryStringValue } from './queryString';
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const useQueryString = (key: string | number, initialValue: any) => {
   const [value, setValue] = useState(getQueryStringValue(key) || initialValue);
